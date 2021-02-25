fc092039f4ab7f3b0ca0c7f606564ce984d62dc1
guojinpeng <gjp0609@qq.com>
2021-02-25 11:52:21 +0800

    utilities

---
+++ utilities/src/main/java/com/onysakura/algorithm/utilities/basic/RandomUtils.java
@@ -0,0 +1,40 @@
+package com.onysakura.algorithm.utilities.basic;
+
+import java.util.Random;
+
+public class RandomUtils {
+    private static final Random RANDOM = new Random();
+
+    public static boolean nextBoolean() {
+        return RANDOM.nextBoolean();
+    }
+
+    public static String randomNumber(int count) {
+        return randomStr(count, StrType.NUMBER);
+    }
+
+    public static String randomStr(int count) {
+        return randomStr(count, StrType.NUMBER, StrType.STRING, StrType.STRING_UPPERCASE);
+    }
+
+    public static String randomStrLowercase(int count) {
+        return randomStr(count, StrType.NUMBER, StrType.STRING);
+    }
+
+    public static synchronized String randomStr(int count, String... strType) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            String str = strType[RANDOM.nextInt(strType.length)];
+            char c = str.charAt(RANDOM.nextInt(str.length()));
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    interface StrType {
+        String NUMBER = "0123456789";
+        String STRING = "abcdefghijklmnopqrstuvwxyz";
+        String STRING_UPPERCASE = STRING.toUpperCase();
+    }
+
+}
